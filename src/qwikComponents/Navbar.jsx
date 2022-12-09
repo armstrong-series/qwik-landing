c5af89442ca448316d1b42a8c05fa5edf7cc1c69
armstrong-series <enefearmstrong@gmail.com>
2022-12-09 12:29:30 +0100

    adding logo

--- src/qwikComponents/Navbar.jsx
+++ src/qwikComponents/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { HiMenu, HiX } from 'react-icons/hi'
+import logo from '../qwikAssets/qwikcountr.png';
 
 export const Navbar = () => {
     const [navigationMenu, setNavigationMenu] = useState(false);
@@ -9,7 +10,8 @@ export const Navbar = () => {
     <div className='w-screen h-[80px] z-10 bg-white fixed'>
         <div className='px-2 flex justify-between items-center w-full h-full'>
             <div className='flex items-center px-5'>
-                <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Qwikcountr</h1>
+                <img src={logo} alt="logo" className='w-15 h-7' />
+                {/* <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>Qwikcountr</h1> */}
                 <ul className='hidden md:flex'>
                     <li>Home</li>
                     <li>Features</li>
